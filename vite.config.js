c61fd6ee0f50d377956de1a4bb6893fa25ab8487
Kiaizen <kiaizenx@gmail.com>
2024-07-15 18:57:39 -0300

    Mudando viteconfig de novo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import vercel from 'vite-plugin-vercel';
- 
+
 export default defineConfig({
+  plugins: [reactRefresh(), vercel()],
   server: {
     port: process.env.PORT,
   },
-  plugins: [vercel()],
 });
