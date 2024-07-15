1d87eb977850199c60b560909d4e5cceb157ddba
Kiaizen <kiaizenx@gmail.com>
2024-07-15 16:18:17 -0300

    Tentativa

--- api/server.js
+++ api/server.js
@@ -7,7 +7,10 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.listen(3000, () => {
   console.log(`JSON Server is running on port 3000`);
 });
