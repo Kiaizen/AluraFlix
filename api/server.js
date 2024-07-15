8f2bb6272c081a336998b779bf29f83c58bb18b4
Kiaizen <kiaizenx@gmail.com>
2024-07-15 17:24:32 -0300

    Tentando com chatgpt

--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 import jsonServer from "json-server";
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router("../db.json");
+const router = jsonServer.router(join(__dirname, '../db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
